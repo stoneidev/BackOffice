4d81566e5855b5385eb18b7345a3d0863b86e3e6
stoenidev <nijin39@gmail.com>
2024-10-10 07:01:13 +0900

    Swagger Conf position change

--- src/main/java/com/stoneistudio/lds/framework/configuration/SwaggerConfig.java
+++ src/main/java/com/stoneistudio/lds/framework/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.stoneistudio.lds.infrastructure.configuration;
+package com.stoneistudio.lds.framework.configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
